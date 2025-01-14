04a9e424f422f60e62688876bd48f68469146f13
kelechi-aims <kelechi.denise@yahoo.com>
2025-01-14 10:38:41 +0100

    added a new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
@@ -51,5 +53,5 @@ export default {
     },
     
   },
-  plugins: [require("tailwind-scrollbar")],
+  plugins: [scrollbar],
 }
