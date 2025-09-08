d152fcd70c8120f300c64f17e866454c55b4c709
kelechi-aims <kelechi@aims.edu.gh>
2025-09-08 15:10:26 +0100

    setup backend with typescript and basic CI/CD

---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+//Midleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Mobiholder backend is running with TypeScript 🚀");
+});
+
+console.log("MONGO_URI from env:", process.env.MONGO_URI);
+
+// Connect to MongoDB
+connectDB();
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
